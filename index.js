64c2d8bdf6515895e7f1e8f46f5ee10a66bc8366
NikolaJacques <44374495+NikolaJacques@users.noreply.github.com>
2022-05-09 16:23:28 +0200

    initial commit

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
-var express = require('express');
-var cors = require('cors');
+const express = require('express');
+const cors = require('cors');
 require('dotenv').config()
+const multer  = require('multer')
+const bodyParser = require('body-parser')
+const upload = multer({ dest: '/uploads' })
 
 var app = express();
 
@@ -11,10 +14,25 @@ app.get('/', function (req, res) {
     res.sendFile(process.cwd() + '/views/index.html');
 });
 
-
-
-
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
   console.log('Your app is listening on port ' + port)
 });
+
+// solution
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+app.post('/api/fileanalyse', upload.single('upfile'), (req,res) => {
+    try {
+        res.json({
+            name: req.file.originalname,
+            type: req.file.mimetype,
+            size: req.file.size
+        })
+    }
+    catch (error){
+        res.send(`the following error occurred: ${error}`)
+    }
+})
